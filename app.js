b94f5b283ae74f7ff9c1a7e608a8b9b10c7c4550
nai golf <niy_golf@hotmail.com>
2018-11-04 00:39:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,19 @@
 // Reply with two static messages
 
+
+
+
+
+
+
+
+
+
 const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
 const app = express()
 const port = process.env.PORT || 4000
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.get('/',function (req, res) {
-    res.end("ok bot")
-    })
-app.post('/webhook', (req, res) => {
-    let reply_token = req.body.events[0].replyToken
-    reply(reply_token)
-    res.sendStatus(200)
-    res.end("ok")
-})
+app.post('/webhook', (req, res) => res.sendStatus(200))
 app.listen(port)
-function reply(reply_token) {
-    let headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer {gT/pAoYEcgwzuM0ifUg2J8ku53kgumr3HyI3gDP3nWbJCXpzQnRZ6kNxQwmVz3hMXFlVSHLg0wh4sH4jOAX+hMy9qNEzCuRO2/+degRUXosrD/MK8kXa9I4TqkV6dVmVWDtTwNS/x/37mnrh5msYdQdB04t89/1O/w1cDnyilFU=}'
-    }
-    let body = JSON.stringify({
-        replyToken: reply_token,
-        messages: [{
-            type: 'text',
-            text: 'Hello'
-        },
-        {
-            type: 'text',
-            text: 'How are you?'
-        }]
-    })
-    request.post({
-        url: 'https://api.line.me/v2/bot/message/reply',
-        headers: headers,
-        body: body
-    }, (err, res, body) => {
-        console.log('status = ' + res.statusCode);
-    });
-}
+
+
+
