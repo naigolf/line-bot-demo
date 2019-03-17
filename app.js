6b26686f21f0143047593898e79ab211981e27ad
nai golf <niy_golf@hotmail.com>
2019-03-18 01:01:45 +0700

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function reply(reply_token) {
         replyToken: reply_token,
         messages: [{
             type: 'text',
-            text: tex_t
+            text: 'ok'
         }]
     })
     request.post({
